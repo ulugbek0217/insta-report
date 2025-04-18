4a79df5fbf173cca126747e323388d0e064c3e52
Ulug'bek Raxmankulov <96826793+ulugbek0217@users.noreply.github.com>
2025-04-18 10:59:57 +0500

    Update data.py

--- data.py
+++ data.py
@@ -2,8 +2,8 @@
 # accounts provide the code with necessary accounts using which the code will report the target user
 # fill the field with accounts [username : password]
 accounts = {
-    "qwer.ty14706": "Mexroj17uz!!!",
+    "username-to-log-in": "password",
 }
 
 # target_user is the target user that has to be reported
-target_user = "q_werty1231"
+target_user = "username-of-target-user"
